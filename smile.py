56d4a63429194d07c9b13a3a2458b1da5fa4c329
sarthakmishraa <56118819+sarthakmishraa@users.noreply.github.com>
2021-05-14 11:36:20 +0530

    Add files via upload

---
+++ smile.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+
+face_cascade = cv.CascadeClassifier('K:/python/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+eye_cascade = cv.CascadeClassifier('K:/python/Lib/site-packages/cv2/data/haarcascade_eye.xml')
+smile_cascade = cv.CascadeClassifier('K:/python/Lib/site-packages/cv2/data/haarcascade_smile.xml')
+
+cap = cv.VideoCapture(0)
+
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.8, 20)
+        for (sx,sy,sw,sh) in smiles:
+            cv.rectangle(roi_color, (sx,sy), (sx+sw, sy+sh), (255,0,0), 2)
+    return frame
+
+while True:
+    ret, frame = cap.read()
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    canvas = detect(gray, frame)
+
+    cv.imshow('Smile', canvas)
+
+    if cv.waitKey(1) & 0xFF==ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
